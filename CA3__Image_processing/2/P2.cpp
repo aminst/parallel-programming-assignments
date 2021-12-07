6e15a645901482762d262e37142decea5b018782
moyarka <mahyar.karimi2079@gmail.com>
2021-12-07 20:19:28 +0330

    Modify serial solution for P2

--- CA3__Image_processing/2/P2.cpp
+++ CA3__Image_processing/2/P2.cpp
@@ -16,12 +16,11 @@ timeval serial(cv::Mat img1, cv::Mat img2)
     unsigned char* img2_data = (unsigned char*) img2.data;
     unsigned char* img_out_data = (unsigned char*) img_out.data;
 
-    for (int i = 0; i < img1.rows; i++) {
-        for (int j = 0; j < img1.cols; j++) {
-            int index = i * img1.cols + j;
-            img_out_data[index] = img2_data[index] + img1_data[index];
-        }
-    }    
+    for (int i = 0; i < img1.rows * img1.cols; i++) {
+        img_out_data[i] = img2_data[i] + img1_data[i];
+        if (img_out_data[i] < img2_data[i])
+            img_out_data[i] = -1;
+    }
 
     gettimeofday(&end, NULL);
     print_time(start, end, false);

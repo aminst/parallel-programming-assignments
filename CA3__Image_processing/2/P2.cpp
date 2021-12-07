6c515a76d9b13573446190c12126f6aaa026423c
aminst <aminst79@gmail.com>
2021-12-07 19:33:02 +0330

    Complete part2

--- CA3__Image_processing/2/P2.cpp
+++ CA3__Image_processing/2/P2.cpp
@@ -1,20 +1,41 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include <opencv4/opencv2/highgui.hpp>
 
+#include "utility.hpp"
+
 using namespace std;
 
-void show_image(cv::Mat image, string name) {
-    cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
-    cv::imshow(name, image);
-    cv::waitKey(0);
+timeval serial(cv::Mat img1, cv::Mat img2)
+{
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    cv::Mat img_out(img1.rows, img1.cols, CV_8U);
+    
+    unsigned char* img1_data = (unsigned char*) img1.data;
+    unsigned char* img2_data = (unsigned char*) img2.data;
+    unsigned char* img_out_data = (unsigned char*) img_out.data;
+
+    for (int i = 0; i < img1.rows; i++) {
+        for (int j = 0; j < img1.cols; j++) {
+            int index = i * img1.cols + j;
+            img_out_data[index] = img2_data[index] + img1_data[index];
+        }
+    }    
+
+    gettimeofday(&end, NULL);
+    print_time(start, end, false);
+
+    cv::imwrite("serial.png", img_out);
+
+    return end - start;
 }
 
-int main()
+timeval parallel(cv::Mat img1, cv::Mat img2)
 {
-    cv::Mat img1 = cv::imread("1.png", cv::IMREAD_GRAYSCALE);
-    cv::Mat img2 = cv::imread("2.png", cv::IMREAD_GRAYSCALE);
-    img2 = img2 * 0.5;
-    
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
     __m128i *img1_src = (__m128i *)img1.data;
     __m128i *img2_src = (__m128i *)img2.data;
 
@@ -36,5 +57,31 @@ int main()
 
     char* temp = (char*)img_out;
     cv::Mat img_out_mat(img1.rows, img1.cols, CV_8UC1, temp);
-    show_image(img_out_mat, "diff");
+
+    gettimeofday(&end, NULL);
+    print_time(start, end, true);
+
+
+    cv::imwrite("parallel.png", img_out_mat);
+
+    return end - start;
+}
+
+int main()
+{
+    cout << "##########################" << endl;
+    cout << "Group Members:" << endl;
+    cout << "Mahyar Karimi: 810197690" << endl;
+    cout << "Amin Setayesh: 810197523" << endl;
+    cout << "##########################" << endl << endl;
+
+    cv::Mat img1 = cv::imread("1.png", cv::IMREAD_GRAYSCALE);
+    cv::Mat img2 = cv::imread("2.png", cv::IMREAD_GRAYSCALE);
+    img2 = img2 * 0.5;
+    
+    timeval serial_time = serial(img1, img2);
+    timeval parallel_time =  parallel(img1, img2);
+
+    print_speedup(serial_time, parallel_time);
+
 }
